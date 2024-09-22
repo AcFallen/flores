d76dbb80fa73d13e650dca72723b699363fddc78
Roberto <r.apaza.cornejo@gmail.com>
2024-09-21 23:13:34 -0500

    deploy

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 onload = () => {
   document.body.classList.remove("container");
 };
+
+document.querySelector('button').addEventListener('click', function() {
+  window.location.href = 'Principal.html';
+});
